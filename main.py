2b4cb5ea1998570cb8ac4a071a4dbc8491f11666
reiem <reiematza@gmail.com>
2023-10-16 08:02:19 +0300

    nicer design

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from nicegui import ui
 app = FastAPI()
 ui.run_with(app)
 
+
 async def create_response(text: ui.textarea):
     video_text = text.value
     text.value = "doing stuff..."
@@ -16,13 +17,13 @@ async def create_response(text: ui.textarea):
     ui.notify("Taj make me kids")
 
 
-
 @ui.page('/')
 def main():
-    with ui.row().classes('w-full  h-2/3 justify-center'):
-        with ui.column().classes('w-1/2 items-center'):
-            ui.label("Auto report generator").classes('text-4xl')
+    with ui.row().classes('w-full  justify-center'):
+        with ui.column().classes('w-1/2 h-full place-center items-center'):
+            ui.label("Auto Report Generator").classes('text-4xl')
             text = ui.textarea(label='Text', placeholder='Enter the video text here').classes('w-full')
-            ui.button('Click me!', on_click=partial(create_response,text)).classes('self-end')
+            ui.button('Click me!', on_click=partial(create_response, text)).classes('self-end')
+
 
 uvicorn.run(app, host="0.0.0.0")
